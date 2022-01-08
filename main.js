1b6a7e497b9a080b95e558af9bd93649fce1af61
BedPled <vishniakov.ae@students.dvfu.ru>
2022-01-08 17:21:32 +1000

    1234

---
+++ main.js
@@ -0,0 +1,35 @@
+let chat = document.getElementById('chat')
+let textInput = document.getElementById('textInput')
+let send = document.getElementById('send')
+
+function scrollMessagesToBottom() {
+    chat.scrollTop = chat.scrollHeight
+}
+
+function onSend() {
+    let message = textInput.value
+
+    if (message) {
+        document.getElementById('chat').innerHTML += `<div class="message" style="justify-content: right">
+                                                                       <div class="message__to">${message}</div>
+                                                                   </div>`
+        textInput.value = ''
+        scrollMessagesToBottom()
+
+
+
+        setTimeout(() => {
+            document.getElementById('chat').insertAdjacentHTML(
+                'beforeend',
+                `<div class="message message__from" style="justify-content: left">
+                    pupa
+                  </div>`
+            );
+        }, 1000);
+
+    }
+}
+
+scrollMessagesToBottom()
+
+send.addEventListener('click', onSend)
